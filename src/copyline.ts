c8f7bcc6a37a54aa24e4a7f9efb690611561c57d
ARA Toshiaki (Univ.) <toshiaki.ara@gmail.com>
2023-10-28 16:48:41 +0900

    update regexp

--- src/copyline.ts
+++ src/copyline.ts
@@ -1,5 +1,20 @@
 import * as vscode from "vscode";
 
+// remove leading bullet point symbol
+const regexp1 = /^\s*(-|\+|\*|\(?[A-Za-z0-9]*[\)?|\.?])(\s+.+)/;
+
+// remove leading half-width spaces and tabs
+const regexp2 = /^[\x20\t]*(.+)/;
+// \x20: half-width spaces
+// \u3000: full-width spaces
+
+// remove last half-width spaces and tabs
+const regexp3 = /[\x20\t]+$/;
+
+// remove last symbols
+const regexp4 = /^(.+?)\s+(#*|[○×]*)$/;
+
+
 
 export function getLineText(editor: vscode.TextEditor): string {
   const position = editor.selection.active.line;
@@ -8,14 +23,11 @@ export function getLineText(editor: vscode.TextEditor): string {
 };
 
 
-export function matchStr(text: string): string {
-  const regexp1 = /^\s*(-|\+|\*|\(?[A-Za-z0-9]*\)?\.?)\s+(.+)/;
-  const regexp2 = /^(.+?)\s*$/;
-  const regexp3 = /^(.+?)\s+(#*|[○×]*)$/;
-
-  const match = text.replace(regexp1, "$2")
-                    .replace(regexp2, "$1")
-                    .replace(regexp3, "$1");
+export function matchStr(str: string): string {
+  const match = str.replace(regexp1, "$2")
+                   .replace(regexp2, "$1")
+                   .replace(regexp3, "")
+                   .replace(regexp4, "$1");
   return match as string;
 };
 
